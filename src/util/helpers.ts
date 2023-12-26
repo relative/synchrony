b3382dce23a2d8356c6340f48de48bff9d97facf
LAADHARI <pabx06@gmail.com>
2023-12-26 16:59:26 +0100

    Update helpers.ts fix for #134
    fix for #134

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -3,7 +3,7 @@ import { Node, EmptyStatement, Literal, Identifier } from './types'
 
 // Immutability™™™™™™
 export function immutate(item: any) {
-  return JSON.parse(JSON.stringify(item))
+  return {...item}
 }
 
 export function literalOrIdentifierToString(node: Node): string {
